b52f5bdadac239ba50936bb165a546119220c8be
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-10-12 21:34:42 +0000

    Added Header to the Restaurant Detailed PAge

--- src/screens/RestaurantDetailsScreen/index.js
+++ src/screens/RestaurantDetailsScreen/index.js
@@ -3,7 +3,8 @@ import DishListItem from "../../components/DishListItem";
 import restaurants from '../../../assets/data/restaurants.json';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { StatusBar } from "expo-status-bar";
-import Header from './Header'
+import Header from './Header';
+import styles from './styles'
 
 const restaurant = restaurants[0];
 const RestaurantDetailPage = () => {
@@ -28,68 +29,5 @@ const RestaurantDetailPage = () => {
 
     )
 }
-
-
-
-const styles = StyleSheet.create({
-    page: {
-        flex: 1,
-    },
-    image: {
-        width: '100%',
-        aspectRatio: 5 / 3,
-    },
-
-    iconContainer: {
-        backgroundColor: 'transparent',
-        position: 'absolute',
-        top: 40,
-        left: 15,
-    },
-    title: {
-        fontSize: 28,
-        fontWeight: '700',
-        marginVertical: 5,
-    },
-    subtitle: {
-        color: '#525252',
-        fontWeight: "500",
-        fontSize: 16,
-    },
-    textContainer: {
-        margin: 15,
-
-    },
-    detailRating: {
-        marginLeft: 'auto',
-        backgroundColor: 'black',
-        padding: 5,
-        width: 80,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 15,
-        position: 'absolute',
-        left: '75%',
-        top: '23%',
-    },
-    ratings: {
-        fontWeight: 'bold',
-        color: 'white',
-        fontSize: 16,
-    },
-    menuCaption: {
-        fontWeight: 'bold',
-        fontSize: 16,
-        color: 'green',
-        marginTop: -5,
-        marginBottom: 1,
-        paddingLeft: 15,
-        textTransform: 'uppercase',
-        lineHeight: 25,
-        textAlign: 'left',
-        letterSpacing: 1,
-    },
-
-})
 
 export default RestaurantDetailPage;
---
+++ src/screens/RestaurantDetailsScreen/styles.js
@@ -0,0 +1,62 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    page: {
+        flex: 1,
+    },
+    image: {
+        width: '100%',
+        aspectRatio: 5 / 3,
+    },
+
+    iconContainer: {
+        backgroundColor: 'transparent',
+        position: 'absolute',
+        top: 40,
+        left: 15,
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: '700',
+        marginVertical: 5,
+    },
+    subtitle: {
+        color: '#525252',
+        fontWeight: "500",
+        fontSize: 16,
+    },
+    textContainer: {
+        margin: 15,
+
+    },
+    detailRating: {
+        marginLeft: 'auto',
+        backgroundColor: 'black',
+        padding: 5,
+        width: 80,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 15,
+        position: 'absolute',
+        left: '75%',
+        top: '23%',
+    },
+    ratings: {
+        fontWeight: 'bold',
+        color: 'white',
+        fontSize: 16,
+    },
+    menuCaption: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        color: 'green',
+        marginTop: -5,
+        marginBottom: 1,
+        paddingLeft: 15,
+        textTransform: 'uppercase',
+        lineHeight: 25,
+        textAlign: 'left',
+        letterSpacing: 1,
+    },
+
+})

95d9028f75dc1c8c0436dc80c6e77360cf8fef9d
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-10-12 23:40:58 +0000

    Adding the Dish Detail Screen

--- src/screens/DishDetailScreen/index.js
+++ src/screens/DishDetailScreen/index.js
@@ -1,20 +1,37 @@
 import { View, Text, StyleSheet } from "react-native";
 import restaurants from "../../../assets/data/restaurants.json";
-import {AntDesign} from '@expo/vector-icons';
-
+import { AntDesign } from '@expo/vector-icons';
+import { useState } from "react";
 
 const dish = restaurants[0].dishes[0];
 
-const  DishDetailScreen = () => {
-    return(
+const DishDetailScreen = () => {
+    const [quantity, setQuantity] = useState(1);
+
+    const onMinus = () => {
+        if (quantity > 1) {
+            setQuantity(quantity - 1)
+        }
+    }
+
+    const onPlus = () => {
+        setQuantity(quantity + 1)
+    }
+
+    return (
         <View style={styles.page}>
             <Text style={styles.name}>{dish.name}</Text>
             <Text style={styles.description}>{dish.description}</Text>
-            <View style={styles.separator}/>
+            <View style={styles.separator} />
 
-            <View>
-                <AntDesign name="minus" size = {50} color="#000"/>
-                <AntDesign name="plus" size = {50} color="#000"/>
+            <View style={styles.row}>
+                <AntDesign name="minus" size={45} color="#000" onPress={onMinus} />
+                <Text style={styles.quantity}>{quantity}</Text>
+                <AntDesign name="plus" size={40} color="#000" onPress={onPlus} />
+            </View>
+
+            <View style={styles.button}>
+                <Text style={styles.buttonText}>Add {quantity} Items To Basket </Text>
             </View>
         </View>
 
@@ -22,28 +39,52 @@ const  DishDetailScreen = () => {
 }
 
 const styles = StyleSheet.create({
-    page:{
+    page: {
         flex: 1,
         width: '100%',
         paddingVertical: 45,
         padding: 15,
     },
-    name:{
+    name: {
         fontSize: 30,
         fontWeight: '600',
         marginVertical: 10,
         textAlign: 'center',
     },
-    description:{
+    description: {
         color: '#696969',
         textAlign: 'center'
     },
-    separator:{
+    separator: {
         height: 1,
         backgroundColor: 'lightgrey',
         marginVertical: 10,
-        borderRadius: 20,
+        // borderRadius: 20,
     },
-})
+    row: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: '65%',
+    },
+    quantity: {
+        fontSize: 25,
+        fontWeight: '600',
+        color: '#696969',
+        marginHorizontal: 20,
+    },
+    button:{
+        backgroundColor: 'black',
+        marginTop: 'auto',
+        padding: 20,
+        alignItems: 'center',
+        borderRadius: 50,
+    },
+    buttonText:{
+        color: 'white',
+        fontSize: 18,
+        fontWeight: '600'
+    },
+});
 
 export default DishDetailScreen;

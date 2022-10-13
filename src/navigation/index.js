3926ffb7344c48cbe0af588eb959a3dbcb830465
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-10-13 17:44:52 +0000

    Routing the navigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,11 +1,17 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import RestaurantDetailPage from './src/screens/RestaurantDetailsScreen';
+import DishDetailScreen from './src/screens/DishDetailScreen';
+import Basket from './src/screens/Basket';
+import OrderScreen from './src/screens/OrderScreen';
+import OrderDetails from './src/screens/OrderDetails';
 
 const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
     return(
         <Stack.Navigator>
-            
+            <Stack.Screen name />
         </Stack.Navigator>
     )
 }

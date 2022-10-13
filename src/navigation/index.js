f87c4e4cac9b66fc1cbbf4fdfbdf8d30432ebf03
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-10-13 18:58:03 +0000

    Material Bottom Tabs added

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,4 +1,5 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
 import HomeScreen from '../screens/HomeScreen';
 import RestaurantDetailPage from '../screens/RestaurantDetailsScreen';
 import DishDetailScreen from '../screens/DishDetailScreen';
@@ -9,14 +10,14 @@ import OrderDetails from '../screens/OrderDetails';
 const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
-    return(
+    return (
         <Stack.Navigator initialRouteName='Home'>
             <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen 
-            name="Restaurants" 
-            component={RestaurantDetailPage} 
-            options={{headerShown: false}}/>
-            <Stack.Screen name="Order" component={Basket}/>
+            <Stack.Screen
+                name="Restaurants"
+                component={RestaurantDetailPage}
+                options={{ headerShown: false }} />
+            <Stack.Screen name="Order" component={Basket} />
             {/* <Stack.Screen name="" component={}/> */}
         </Stack.Navigator>
     )

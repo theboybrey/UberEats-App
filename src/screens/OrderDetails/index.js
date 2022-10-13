11958f0f08a9393966b1bcc9fd7639ebbd907bd4
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-10-13 12:29:56 +0000

    Added the Order Details Screen

---
+++ src/screens/OrderDetails/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const OrderDetails   = () => {
+  return (
+    <View>
+      <Text>OrderDetails  </Text>
+    </View>
+  )
+}
+
+export default OrderDetails  

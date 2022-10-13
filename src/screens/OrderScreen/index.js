74e8e6ca1f48c5d78ecea789d03186a864bd9086
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-10-13 11:22:52 +0000

    Added the order screen

--- src/screens/OrderScreen/index.js
+++ src/screens/OrderScreen/index.js
@@ -1,12 +1,12 @@
     import { View, Text } from 'react-native'
     import React from 'react'
     
-    const index = () => {
+    const OrderScreen = () => {
       return (
         <View>
-          <Text>index</Text>
+          <Text>OrderScreen</Text>
         </View>
       )
     }
     
-    export default index
+    export default OrderScreen

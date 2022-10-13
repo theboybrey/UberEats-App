1dbfe3ee8f38de4da56f5daee200df8dd122cb8e
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-10-13 13:08:23 +0000

    STyling the Order History Components

--- src/screens/OrderDetails/index.js
+++ src/screens/OrderDetails/index.js
@@ -1,12 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { StatusBar } from 'expo-status-bar';
+import { View, Text, Image } from 'react-native'
+import orders from '../../../assets/data/orders.json'
+import styles from './styles';
 
-const OrderDetails   = () => {
-  return (
-    <View>
-      <Text>OrderDetails  </Text>
-    </View>
-  )
+const order = orders[0];
+
+const OrderDetails = () => {
+    return (
+        <View>
+            <StatusBar style="light" />
+
+            <View style={styles.page}>
+                <Image source={{ uri: order.Restaurant.image }} style={styles.image}></Image>
+
+                {/* Ratings on the Detailed Page */}
+                <View style={styles.detailRating}>
+                    <Text style={styles.ratings}>{order.Restaurant.rating.toFixed(1)}</Text>
+                </View>
+
+                <View style={styles.textContainer}>
+                    <Text style={styles.title}>{order.Restaurant.name}</Text>
+                    <Text style={styles.subtitle}>{order.status} &#8226; 2 days ago</Text>
+                </View>
+                <Text style={styles.menuCaption}>Your Order History</Text>
+
+
+
+
+            </View>
+
+        </View>
+    )
 }
 
-export default OrderDetails  
+export default OrderDetails
